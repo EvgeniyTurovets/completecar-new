fea0acd53db3ac6baa25c2365bfcb802bbac965b
sanyokstd <sanyokstd@gmail.com>
2022-02-16 18:47:08 +0200

    1
    1

---
+++ app/js/main.js
@@ -0,0 +1,49 @@
+"use strict";
+
+new WOW().init();
+lazyload();
+"use strict";
+
+$(function () {
+  function headerFix() {
+    var scroll = $(document).scrollTop();
+    var offsetTop = $('.header__top').height();
+
+    if (scroll > offsetTop) {
+      $('.header__fix').addClass('active');
+    } else {
+      $('.header__fix').removeClass('active');
+    }
+  }
+
+  headerFix();
+
+  function srollTopFade() {
+    if ($(document).scrollTop() > 300) {
+      $('#toTop').addClass('active');
+    } else {
+      $('#toTop').removeClass('active');
+    }
+  }
+
+  srollTopFade();
+  $('#toTop').on('click', function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+  });
+  $(document).on('scroll', function () {
+    headerFix();
+    srollTopFade();
+  });
+  var swiper = new Swiper('#sliders-container', {
+    loop: true,
+    effect: 'fade',
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+});
+//# sourceMappingURL=main.js.map
